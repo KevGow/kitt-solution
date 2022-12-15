654f9ee445f2f3e8903d68ee3d2467c38fdc89f2
KevGow <113835094+KevGow@users.noreply.github.com>
2022-12-15 18:37:35 +0000

    Refactor: Added comments
    Added comments to code.

--- kitt-solution.go
+++ kitt-solution.go
@@ -8,29 +8,43 @@ import (
 	"strconv"
 )
 
-const pPM = 2         //Price per minute
-const pPH = 22        //Price per hour
-const pPD = 60        //Price per day
-const pPW = 105       //Price per week
-const minsInHour = 60 //per hour
-const minsInDay = 1440
-const minsInWeek = 10080
+const pPM = 2            //Price per minute
+const pPH = 22           //Price per hour
+const pPD = 60           //Price per day
+const pPW = 105          //Price per week
+const minsInHour = 60    //Minutes per hour
+const minsInDay = 1440   //Minutes per day
+const minsInWeek = 10080 //Minutes per Week
 
 func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("Expected a single argument to be supplied for the meeting time eg. 1400")
+	}
+
 	meetingTime := os.Args[1] //set meeting time in minutes
+
 	meetingTimeInt, err := strconv.Atoi(meetingTime)
 	if err != nil {
 		log.Fatalf("Could not convert %s to int. Please supply a valid integer", meetingTime)
 	}
+
 	price := GetPrice(meetingTimeInt)
+
 	fmt.Printf("%d minutes = £%d\n", meetingTimeInt, price)
 }
 
 func GetPrice(meetingTime int) int {
+
+	//There are 6 conditions we need to check for. If the meeting time is less than a full unit's rate (eg. 23 hours is less than a day rate),
+	//the rentee can pay for a full day or week, but not hour or minute. These are the first 3 logical checks. The next 3 checks check that
+	//a meeting time is greater than a full unit's rate (eg. 3 hours is greater than 1 hour), the rentee can use the hourly rate for the 3
+	//hour duration.
+
+	//If a check is passed, the price will be added to an int slice. This int slice contains all the possible prices, which is then orded.
+	//The lowest price in the slice is then returned by the function.
+
 	var prices []int
 
-	//If the Meeting Time is less than a particular rate, that rate can be paid.
-	//eg: If
 	if meetingTime <= minsInWeek {
 		prices = append(prices, pPW)
 	}
@@ -41,7 +55,7 @@ func GetPrice(meetingTime int) int {
 		prices = append(prices, pPH)
 	}
 
-	prices = append(prices, pPM*meetingTime)
+	prices = append(prices, pPM*meetingTime) //always add this price, as you can always pay by the minute rate
 
 	if meetingTime > minsInHour {
 		prices = append(prices, pPH*meetingTime/minsInHour)
@@ -52,6 +66,8 @@ func GetPrice(meetingTime int) int {
 	if meetingTime > minsInWeek {
 		prices = append(prices, pPW*meetingTime/minsInWeek)
 	}
+
 	sort.Ints(prices)
+
 	return prices[0]
 }
